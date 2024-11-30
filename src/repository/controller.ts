57fcdc1e35e0501dd8be017e44e618092003ce4f
onlyhappiness <onlyhappiness96@gmail.com>
2024-11-30 22:52:23 +0900

    test: request body 값 확인

--- src/repository/controller.ts
+++ src/repository/controller.ts
@@ -1,5 +1,4 @@
 import { NextFunction, Request, Response } from 'express';
-import { GithubService } from '../github/service';
 
 export class RepositoryController {
   static async getHello(req: Request, res: Response, next: NextFunction) {
@@ -14,13 +13,17 @@ export class RepositoryController {
   static async handleWebhook(req: Request, res: Response, next: NextFunction) {
     try {
       const payload = req.body;
+      console.log('payload:::::', payload);
 
-      const { repositoryName, message } =
-        await GithubService.formatWebhookMessage(payload);
+      // const { repositoryName, message } =
+      //   await GithubService.formatWebhookMessage(payload);
 
-      console.log(repositoryName, message);
+      // console.log(repositoryName, message);
 
-      return res.status(200).send({ repositoryName, message });
+      // return res.status(200).send({ repositoryName, message });
+      return res.status(200).send({
+        body: payload,
+      });
     } catch (error) {
       next(error);
     }
